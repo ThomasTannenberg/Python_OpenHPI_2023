7d7fc7ac408f484038b5ce99bf1a426fb7655291
Thomas Tannenberg <114718959+ThomasTannenberg@users.noreply.github.com>
2023-09-16 16:12:15 +0200

    Add files via upload

---
+++ Python20 Snake-Spiel 3 Aufgabe 1.py
@@ -0,0 +1,13 @@
+from turtle import *
+from definitionen import *
+
+def spiel_neustarten():
+
+
+    kopf.goto(0, 0)
+    kopf.direction = 'stop'
+    
+
+    segmente_entfernen()
+    
+    print('Spiel ist vorbei')
